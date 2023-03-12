3b3c1c23088d4519dbe6cfbd1006097a3f6431e9
m.kamwana <maxwell.kamwana@gmail.com>
2023-03-12 22:31:22 +0000

    work in progress

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "815acc3e907f4a1d1b08a2c99ee208ae"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "9f84d5c8b1b44ba19c9988e42bfa9914"
